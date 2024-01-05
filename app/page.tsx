a758097eda92d62b0afefcb9d087c144a92a9d3a
dsl2022 <2015rpro@gmail.com>
2024-01-04 23:59:36 -0500

    added homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Image from 'next/image'
+import Head from "next/head"
 
 export default function Home() {
   return (
+    <>
+       <Head>
+        <title>WebShell</title>
+        <link rel="stylesheet" href="src/css/css-reset.css"/>
+        <link rel="stylesheet" href="src/css/style.css"/>
+        <link rel="icon" type="image/x-png" href="/res/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1" />
+        <script src="https://kit.fontawesome.com/d5effce557.js" crossOrigin="anonymous"></script>
+      </Head>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
@@ -109,5 +119,6 @@ export default function Home() {
         </a>
       </div>
     </main>
+    </>
   )
 }
